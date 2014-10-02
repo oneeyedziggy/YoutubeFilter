e35570a788ebccde36c4647f5360a9045ae061d5
oneeyedziggy <oneeyedziggy@gmail.com>
2014-10-01 22:26:17 -0700

    Update Filter.js
    pulled the 'tubers' list dynamically, made 'tubersBlackWordsConfig' lists case insensitive, added a while loop to keep making passes in case filters aren't all in exactly the videos occur.

--- Filter.js
+++ Filter.js
@@ -1,14 +1,33 @@
-console.clear();
-var tubers = ["mathasgames","paulsoaresjr","yogscast martyn","captainsparklez","generikb","imanderzel","docm77","yogscast lewis & simon"];
+//console.clear();
+//var tubers = ["mathasgames","paulsoaresjr","yogscast martyn","captainsparklez","generikb","imanderzel","docm77","yogscast lewis & simon"];
+var tubersBlackWordsConfig = {};
+tubersBlackWordsConfig["mathasgames"] = ["gauntlet","sims 4","streaming","wealth of nations","neverending nightmares"];
+tubersBlackWordsConfig["yogscast lewis & simon"] = ["hole diggers", "landmark"];
+tubersBlackWordsConfig["docm77"] = ["civilization v"];
+tubersBlackWordsConfig["imanderZEL"] = ["battlefield 4"];
+tubersBlackWordsConfig["generikb"] = ["crash landing"];
+tubersBlackWordsConfig["captainSparklez"] = ["five nights", "minite", "super modded"];
+tubersBlackWordsConfig["yogscast martyn"] = ["tOmoDachi"];
+tubersBlackWordsConfig["paulsoaresjr"] = ["the escapists"];
+
 var tubersBlackWords = {};
-tubersBlackWords["mathasgames"] = ["sims 4","gauntlet","streaming","wealth of nations","neverending nightmares"];
-tubersBlackWords["yogscast lewis & simon"] = ["hole diggers", "landmark"];
-tubersBlackWords["docm77"] = ["civilization v"];
-tubersBlackWords["imanderzel"] = ["battlefield 4"];
-tubersBlackWords["generikb"] = ["crash landing"];
-tubersBlackWords["captainsparklez"] = ["five nights", "minite", "super modded"];
-tubersBlackWords["yogscast martyn"] = ["tomodachi"];
-tubersBlackWords["paulsoaresjr"] = ["the escapists"];
+//convert all tuber names and keywords to lower case
+for(tuber in tubersBlackWordsConfig){
+	for(word in tubersBlackWordsConfig[tuber]){
+    
+	  tubersBlackWordsConfig[tuber][word] = tubersBlackWordsConfig[tuber][word].toLowerCase(); 
+    console.log(tubersBlackWordsConfig[tuber][word]);
+  }
+	tubersBlackWords[tuber.toLowerCase()] = tubersBlackWordsConfig[tuber];
+}
+
+
+tubers = [];
+for(tuber in tubersBlackWords){
+	tubers.push(tuber);
+}
+console.log(tubersBlackWords);
+console.log(tubers);
 //tubersBlackWords[""] = [""];
 //tubersBlackWords[""] = [""];
 //tubersBlackWords[""] = [""];
@@ -27,22 +46,32 @@ var subList = document.getElementById('browse-items-primary');
 var subListElements = subList.getElementsByClassName('feed-item-container');
 
 //console.log(subListElements);
-
-for(element in subListElements){
-  element = subListElements[element];
-  if((typeof element) == 'object'){
-    var tuberName = element.getElementsByClassName('branded-page-module-title-text')[0].childNodes[0].textContent.trim().toLowerCase();
-    console.log('\'' + tuberName + '\''); //
-    var videoTitle = element.getElementsByClassName('yt-uix-tile-link')[0].childNodes[0].data.trim().toLowerCase();
-    //console.log(videoTitle);
-    if(tubers.indexOf(tuberName) >= 0){
-      //tuber is in filter list
-      for(blackWord in tubersBlackWords[tuberName]){
-        blackWord = tubersBlackWords[tuberName][blackWord];
-        //console.log(blackWord);
-        if(blackWord != undefined && videoTitle.indexOf(blackWord) >= 0){
-          element.parentNode.removeChild(element);
-          //console.log(videoTitle);
+var flag = 0;
+//make passes untill all are gone
+while(flag == 0){
+  //flip the 'while' flag as soon as you start an iteration 
+  flag = 1;
+  for(element in subListElements){
+    element = subListElements[element];
+    if((typeof element) == 'object'){
+      var tuberName = element.getElementsByClassName('branded-page-module-title-text')[0].childNodes[0].textContent.trim().toLowerCase();
+      //console.log('\'' + tuberName + '\''); //
+      var videoTitle = element.getElementsByClassName('yt-uix-tile-link')[0].childNodes[0].data.trim().toLowerCase();
+      //console.log(videoTitle);
+      if(tubers.indexOf(tuberName) >= 0){
+        //tuber is in filter list
+        for(blackWord in tubersBlackWords[tuberName]){
+          blackWord = tubersBlackWords[tuberName][blackWord];
+          //console.log(blackWord);
+          console.log(blackWord + ', ' + videoTitle + ', ' + videoTitle.indexOf(blackWord))
+          if(blackWord != undefined && videoTitle.indexOf(blackWord) >= 0){
+            console.log('prev elem deleted');
+            element.parentNode.removeChild(element);
+            //reset the flag icon if an element is remived this pass 
+            flag = 0;
+            break;
+            //console.log(videoTitle);
+          }
         }
       }
     }
